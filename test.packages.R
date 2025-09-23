597618d59b653f9583a7dc68caa8e387020b035f
dbmdvker <kerenkasiamaebaba@gmail.com>
2025-09-23 12:27:30 +0000

    k

--- test.packages.R
+++ test.packages.R
@@ -1,12 +1,4 @@
 library(RPostgres)
-<<<<<<< HEAD
-<<<<<<< HEAD
 library(tidyverse) #Hello world
-=======
-Library(tidyverse)
->>>>>>> a7e8201573ebdffffe25a73cc55cc8265533f1e4
-=======
-Library(tidyverse) #load many packages
->>>>>>> 5fa29e91588f5543c41932f41fcf3ad766963fec
 library(dplyr)
 
